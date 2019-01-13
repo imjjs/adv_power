73c61ebf353d8a4102f89a6acb5bdb3834d2a556
imjjs <liyizhe20@gmail.com>
2019-01-13 03:06:07 -0600

    ddd

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import loader
 import model
 import numpy
+import powerData.deepforge4.getPowerData
 from keras import backend as K
+from keras.models import load_model, Model
+from keras.layers.core import Lambda
+from sklearn.metrics import mean_squared_error
 
 
 def deviation(original, adv):
@@ -13,17 +17,78 @@ def deviation(original, adv):
         ret.append(rd)
     return ret
 
+def multiple_IterativeGSM(model, X_tst, step_length, steps, reverse, grad):
+    x_copy = X_tst.copy()
+    get_grad_values = K.function([model.input], grad)
+    for step in range(steps):
+        # stepLen = step_length * (x_copy.max_value - x_copy.min_value) / steps
+        grad_values = get_grad_values([x_copy])[0]
+        grad_signs = numpy.sign(grad_values)  # get sign
+        noise = grad_signs * step_length  # noise
+        x_copy = x_copy + noise * reverse
+    return x_copy
+
+def set_testing_dropout(model, rate):
+    # Store the fully connected layers
+    layerbefore = model.layers[-2]
+    predictions = model.layers[-1]
+
+    # Create the dropout layers
+    testing_dropout = Lambda(lambda x: K.dropout(x, level=rate))
+
+    # Reconnect the layers
+    x = testing_dropout(layerbefore.output)
+    predictors = predictions(x)
+
+    # Create a new model
+    new_model = Model(input=model.input, output=predictors)
+    #new_model.summary()
+    return new_model
+
+
+def trainsform(original_test_x):
+    ratio = .1
+    z = numpy.zeros(original_test_x.shape)
+    tmp = numpy.random.randint(100, size=original_test_x.shape)
+    indx = numpy.where(tmp < 100 * ratio, original_test_x, z)
+    return indx
+
 if __name__ == '__main__':
-    l = loader.Loader('data_load.csv')
-    ins = l.load()
-    exp = model.Experiment(ins)
-    raw_predictions = exp.predict_test()
-    predictions = [x[0] for x in raw_predictions]
-    #K.set_learning_phase(0)
-    X_advs = exp.attack()
-    X_advs = numpy.reshape(X_advs, (207, 24, 1))
-    exp.set_testing_dropout(.3)
-    raw_adv_predictions = exp.predict(X_advs)
-    adv_predictions = [x[0] for x in raw_adv_predictions]
-    ret = deviation(predictions, adv_predictions)
-    print(sum(ret)/len(ret))
+    dataobj = powerData.deepforge4.getPowerData.GetPowerData()
+    data = dataobj.execute()
+    print(data[3].shape)
+    _, _, test_x, test_y = data
+    model = load_model('powerData/lstm150fc500.h5')
+    #model.summary()
+    res_model = set_testing_dropout(model, .1)
+    grad = K.gradients(model.output, model.input)
+    adv_test_x = multiple_IterativeGSM(model, test_x, .001, 100, 1, grad)
+    result = model.predict(test_x)
+    adv_result = model.predict(adv_test_x)
+    adv_res_test_x = trainsform(adv_test_x)
+    adv_res_result = res_model.predict(adv_res_test_x)
+    rd = deviation(test_y, result)
+    adv_rd = deviation(test_y, adv_result)
+    adv_res_rd = deviation(test_y, adv_res_result)
+    mse = mean_squared_error(test_y, result)
+    adv_mse  = mean_squared_error(test_y, adv_result)
+    adv_res_mse = mean_squared_error(test_y, adv_res_result)
+    print(mse, adv_mse, adv_res_mse)
+    print(sum(rd)/len(rd))
+    print(sum(adv_rd) / len(adv_rd))
+    print(sum(adv_res_rd) / len(adv_res_rd))
+
+
+
+
+    # exp = model.Experiment(ins)
+    # raw_predictions = exp.predict_test()
+    # predictions = [x[0] for x in raw_predictions]
+    # #K.set_learning_phase(0)
+    # X_advs = exp.attack()
+    # X_advs = numpy.reshape(X_advs, (207, 24, 1))
+    # exp.set_testing_dropout(.3)
+    # raw_adv_predictions = exp.predict(X_advs)
+    # adv_predictions = [x[0] for x in raw_adv_predictions]
+    # ret = deviation(predictions, adv_predictions)
+    # print(sum(ret)/len(ret))
--- model.py
+++ model.py
@@ -18,24 +18,10 @@ from keras.layers.core import Lambda
 
 
 class Experiment(object):
-    def __init__(self, _instances: loader.Instance, load_model=True):
-        self.feature_dim = 24
+    def __init__(self, _instances: loader.Instance, _model):
+        self.feature_dim = 24 * 110
         self.instances = _instances
-        self.model = None
-        if load_model is True:
-            yaml_file = open('model.yaml', 'r')
-            loaded_model_yaml = yaml_file.read()
-            yaml_file.close()
-            self.model = model_from_yaml(loaded_model_yaml)
-            # load weights into new model
-            self.model.load_weights("model.h5")
-            self.model.compile(loss='mse',  # mse = MSE = mean_squared_error
-                          optimizer='rmsprop',
-                          metrics=['accuracy'])
-            print("Loaded model from disk")
-        else:
-            self.build_model()
-            self.train(True)
+        self.model = _model
         self.grad = K.gradients(self.model.output, self.model.input)
 
     def set_testing_dropout(self, rate):
@@ -53,50 +39,6 @@ class Experiment(object):
         # Create a new model
         self.model = Model(input=self.model.input, output=predictors)
 
-    def build_model(self):
-        model = Sequential()
-        model.add(Conv1D(self.feature_dim, 3, activation='relu', input_shape=(self.feature_dim, 1)))
-        model.add(Conv1D(self.feature_dim, 3, activation='relu'))
-        model.add(MaxPooling1D(3))
-        model.add(Conv1D(self.feature_dim * 2, 3, activation='relu'))
-        model.add(Conv1D(self.feature_dim * 2, 3, activation='relu'))
-        model.add(GlobalAveragePooling1D())
-        model.add(Dense(self.feature_dim * 7, activation='relu'))
-        model.add(Dropout(0.3))
-        model.add(Dense(1, activation='linear'))
-
-        model.compile(loss='mse',  # mse = MSE = mean_squared_error
-                      optimizer='rmsprop',
-                      metrics=['accuracy'])
-        self.model = model
-    
-    def add_dropout(self,rate):
-        # Store the fully connected layers
-        layerbefore = self.model.layers[-3]
-        predictions = self.model.layers[-1]
-        
-        # Create the dropout layers
-        
-        dropout1 = (Lambda(lambda x: K.dropout(x, level=rate)))
-        
-        # Reconnect the layers
-        x = dropout1(layerbefore.output)
-        predictors = predictions(x)
-        
-        # Create a new model
-        self.model = Model(input=self.model.input, output=predictors)
-
-
-    def train(self, save=True):
-        inp = self.instances
-        self.model.fit(inp.X_train, inp.y_train, batch_size=512, nb_epoch=50, validation_split=0.05, verbose=1)
-        if save is True:
-            model_yaml = self.model.to_yaml()
-            with open("model.yaml", "w") as yaml_file:
-                yaml_file.write(model_yaml)
-            # serialize weights to HDF5
-            self.model.save_weights("model.h5")
-            print("Saved model to disk")
 
     def predict(self, inp_X):
         predicted_values = self.model.predict(inp_X)
