e2371f55d623857ff2ec428066695f84183886e9
imjjs <liyizhe20@gmail.com>
2019-01-20 15:05:36 -0600

    l0 results

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import loader
 import model
 import numpy
+import pickle
 import powerData.deepforge4.getPowerData
 from keras import backend as K
 from keras.models import load_model, Model
@@ -17,7 +18,52 @@ def deviation(original, adv):
         ret.append(rd)
     return ret
 
-def multiple_IterativeGSM(model, X_tst, step_length, steps, reverse, grad):
+
+def l0_attack(model, X_tst, reverse, grad, threshold):
+    ret = []
+    original_prediction = model.predict(X_tst)
+    get_grad_values = K.function([model.input], grad)
+    max_value = X_tst.max()
+    min_value = X_tst.min()
+    shift_value = 5.62481483
+    for i in range(len(X_tst)):
+        print('ins: ' + str(i))
+        x = X_tst[i]
+        x_adv = x.copy()
+        cnt = 0
+        cnt_failure = 0
+        prediction = original_prediction[i][0]
+        adv_prediction = prediction
+        gradient = get_grad_values([numpy.array([x])])[0][0]
+        abs_gradient = numpy.abs(gradient)
+        while (adv_prediction - prediction) * reverse / (prediction + shift_value) <= threshold:
+            argmax_num = numpy.nanargmax(abs_gradient)
+            argmax_indices = numpy.unravel_index(argmax_num, abs_gradient.shape)
+            abs_gradient[argmax_indices] = 0  # to find the next largest gradient pixel
+            if reverse == 1:
+                best_value = max_value if gradient[argmax_indices] > 0 else min_value
+            else:
+                best_value = min_value if gradient[argmax_indices] > 0 else max_value
+            saved_value = x_adv[argmax_indices]
+            flag_better = False
+            for k in numpy.linspace(min_value, max_value, num=10):
+                x_adv[argmax_indices] = k
+                y_pred_tmp = model.predict(numpy.array([x_adv]))[0][0]
+                if (y_pred_tmp - adv_prediction) * reverse > 0:
+                    best_value = k
+                    adv_prediction = y_pred_tmp
+                    flag_better = True
+    #
+            print(cnt, gradient[argmax_indices], argmax_indices, best_value,
+                  adv_prediction, flag_better)
+            x_adv[argmax_indices] = best_value if flag_better else saved_value
+            cnt = cnt + 1 if flag_better else cnt
+        ret.append((x_adv, cnt))
+    return numpy.array(ret)
+
+
+
+def multiple_IterativeGSM(model, X_tst, step_length, steps, reverse, grad, ratio):
     x_copy = X_tst.copy()
     get_grad_values = K.function([model.input], grad)
     for step in range(steps):
@@ -26,6 +72,11 @@ def multiple_IterativeGSM(model, X_tst, step_length, steps, reverse, grad):
         grad_signs = numpy.sign(grad_values)  # get sign
         noise = grad_signs * step_length  # noise
         x_copy = x_copy + noise * reverse
+    # for i in range(len(x_copy)):
+    #     for j in range(len(x_copy[0])):
+    #         tmp = numpy.random.randint(0, 100)
+    #         if tmp < 100 * ratio:
+    #             x_copy[i][j] = X_tst[i][j]
     return x_copy
 
 def set_testing_dropout(model, rate):
@@ -69,26 +120,30 @@ if __name__ == '__main__':
     # model.summary()
     # res_model = set_testing_dropout(model, .1)
     grad = K.gradients(model.output, model.input)
-    adv_test_x = multiple_IterativeGSM(model, test_x, .0002, 100, 1, grad)
-    result = model.predict(test_x)
-    adv_result = model.predict(adv_test_x)
-    adv_res_test_x = trainsform(adv_test_x)
-    adv_res_result = model.predict(adv_res_test_x)
-    res_test_x = trainsform(test_x)
-    res_result = model.predict(res_test_x)
-    rd = deviation(test_y, result)
-    adv_rd = deviation(test_y, adv_result)
-    adv_res_rd = deviation(test_y, adv_res_result)
-    res_rd = deviation(test_y, res_result)
-    mse = mean_squared_error(test_y, result)
-    adv_mse  = mean_squared_error(test_y, adv_result)
-    adv_res_mse = mean_squared_error(test_y, adv_res_result)
-    res_mse = mean_squared_error(test_y, res_result)
-    print(mse, adv_mse, adv_res_mse, res_mse)
-    print(sum(rd)/len(rd))
-    print(sum(adv_rd) / len(adv_rd))
-    print(sum(adv_res_rd) / len(adv_res_rd))
-    print(sum(res_rd)/ len(res_rd))
+    thrs = [.1, .15, .2]
+    for thr in thrs:
+        adv_test_x = l0_attack(model, test_x, -1, grad, thr)
+        with open('min_{pc}%_l0.pk'.format(pc=str(thr*100)), 'wb') as pk:
+            pickle.dump(adv_test_x, pk)
+    # result = model.predict(test_x)
+    # adv_result = model.predict(adv_test_x)
+    # adv_res_test_x = trainsform(adv_test_x)
+    # adv_res_result = model.predict(adv_res_test_x)
+    # res_test_x = trainsform(test_x)
+    # res_result = model.predict(res_test_x)
+    # rd = deviation(test_y, result)
+    # adv_rd = deviation(test_y, adv_result)
+    # adv_res_rd = deviation(test_y, adv_res_result)
+    # res_rd = deviation(test_y, res_result)
+    # mse = mean_squared_error(test_y, result)
+    # adv_mse  = mean_squared_error(test_y, adv_result)
+    # adv_res_mse = mean_squared_error(test_y, adv_res_result)
+    # res_mse = mean_squared_error(test_y, res_result)
+    # print(mse, adv_mse, adv_res_mse, res_mse)
+    # print(sum(rd)/len(rd))
+    # print(sum(adv_rd) / len(adv_rd))
+    # print(sum(adv_res_rd) / len(adv_res_rd))
+    # print(sum(res_rd)/ len(res_rd))
     # for idx in range(len(res_rd)):
     #     print(result[idx], adv_result[idx], res_result[idx])
 
